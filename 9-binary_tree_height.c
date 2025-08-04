829b63f0c46be4f82e0f1db5d7d80395c9320aa6
Saniez-l <saniezl.christophe@gmail.com>
2025-08-04 17:45:58 +0200

    function for return height of binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+
+/**
+ * max - find max
+ * @a: value a
+ * @b: value b
+ * Return: max
+ */
+int max(int a, int b)
+{
+	if (a >= b)
+		return (a);
+
+	else
+		return (b);
+}
+
+/**
+ * height - height binary tree
+ * @tree:  benary tree
+ * Return: height
+ */
+int height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (-1);
+
+	return (max(height(tree->left), height(tree->right) + 1));
+}
+
+
+/**
+ * binary_tree_height - recursive for binary tree
+ * @tree:  benary tree
+ * Return: height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (height(tree));
+}
