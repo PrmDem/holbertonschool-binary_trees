73052d50f705a803a37e20320cac3c7525a1036d
Saniez-l <saniezl.christophe@gmail.com>
2025-08-04 22:01:12 +0200

    update mistake of the function binary tree depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,19 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * depth - function find depth tree binary
- * @tree:  benary tree
- * Return: depth tree->parent
- */
-int depth(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (-1);
-
-	return (depth(tree->parent) + 1);
-}
-
-/**
  * binary_tree_depth - function find depth tree binary
  * @tree:  benary tree
  * Return: depth
@@ -22,7 +9,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
-	return (depth(tree));
+	return (binary_tree_depth(tree->parent) + 1);
 }
